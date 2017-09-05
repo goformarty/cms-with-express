bff66c7e0b674b5a595df24ad0e0d27d9df5b6b0
Marty <marty.stepien@gmail.com>
2017-09-05 13:55:23 +0100

    simple routing added

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
-    console.log('Listening to local port 3000');
+app.get('/', function(req, res) {
+    res.send('Meow wow');
+});
+
+app.get('/marty', function(req, res) {
+    res.send('Hey Marty');
+});
+
+app.listen(3000, function () {
+  console.log('Server is listening on port 3000. Ready to accept requests!');
 });
